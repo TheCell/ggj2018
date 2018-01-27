9fec8ac9ae9af94d5225fa68fe2484829e76f307
micromic5 <mike.amacker@gmx.ch>
2018-01-27 10:40:17 +0100

    modified jump

--- unityProject/polyIsle/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ unityProject/polyIsle/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -63,7 +63,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
         {
             RotateView();
             // the jump state needs to read here to make sure it is not missed
-            if (!m_Jump)
+            if (m_CharacterController.isGrounded)
             {
                 m_Jump = CrossPlatformInputManager.GetButtonDown("Jump");
             }
