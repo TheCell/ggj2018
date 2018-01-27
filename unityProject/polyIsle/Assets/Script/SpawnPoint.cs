602790e54a11100b5bc7579272873e9497c66c90
micromic5 <mike.amacker@gmx.ch>
2018-01-27 12:21:42 +0100

    Added Spawn and kill Triggers

---
+++ unityProject/polyIsle/Assets/Script/SpawnPoint.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+        {
+            other.GetComponent<Player>().spawnPoint = GetComponent<Transform>();
+        }
+    }
+}
