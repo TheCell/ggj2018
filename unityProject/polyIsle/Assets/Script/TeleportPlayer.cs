a8f469cd539cc590f5b60461af73e07c230c0756
micromic5 <mike.amacker@gmx.ch>
2018-01-27 13:03:46 +0100

    startet own scene, change kill collider to kill trigger

---
+++ unityProject/polyIsle/Assets/Script/TeleportPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlayer : MonoBehaviour {
+
+    public float resetX = 0;
+    public float resetY = 0;
+    public float resetZ = 20;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+        {
+            other.GetComponent<Transform>().position = new Vector3(other.GetComponent<Transform>().position.x - resetX, other.GetComponent<Transform>().position.y - resetY, other.GetComponent<Transform>().position.z - resetZ);
+        }
+    }
+}
