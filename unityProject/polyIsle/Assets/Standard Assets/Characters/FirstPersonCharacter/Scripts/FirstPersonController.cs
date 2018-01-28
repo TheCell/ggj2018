749ddd49c83a802f6cb06d31970e4c8424ad93c3
Jony Margelist <jony.margelist@stud.hslu.ch>
2018-01-28 14:30:48 +0100

    changed footsteps, removed water sound explosion, added path to tunnels

--- unityProject/polyIsle/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ unityProject/polyIsle/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -55,6 +55,7 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_Jumping = false;
             m_AudioSource = GetComponent<AudioSource>();
 			m_MouseLook.Init(transform , m_Camera.transform);
+            m_AudioSource.volume = 0.2f;
         }
 
 
